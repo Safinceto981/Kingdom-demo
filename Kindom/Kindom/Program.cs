6c824ea2a11d52ba02b034802c6562df518ac12d
Safinceto981 <safi_981@abv.bg>
2019-10-11 15:08:20 +0300

    First commit-making the project

---
+++ Kindom/Kindom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kindom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
